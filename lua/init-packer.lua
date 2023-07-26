d792430564ef959a2e25b78400a7f6835c913915
shicheng <shichengv@outlook.com>
2023-07-26 12:17:04 +0800

    Changed Keymap

--- lua/init-packer.lua
+++ lua/init-packer.lua
@@ -28,7 +28,6 @@ return require('packer').startup(function(use)
         run = function() vim.fn["mkdp#util#install"]() end,
     })
 
-    -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
 
 
@@ -51,7 +50,7 @@ return require('packer').startup(function(use)
     --
     --
     -- }
-    use "tenxsoydev/karen-yank.nvim"
+    -- use "tenxsoydev/karen-yank.nvim"
 
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
@@ -72,7 +71,7 @@ return require('packer').startup(function(use)
 
     -- TabLine
     -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
-    use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+    -- use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
     use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
     use 'romgrk/barbar.nvim'
 
@@ -89,7 +88,7 @@ return require('packer').startup(function(use)
     -- indent
     use "lukas-reineke/indent-blankline.nvim"
     -- git integration
-    use 'lewis6991/gitsigns.nvim'
+    -- use 'lewis6991/gitsigns.nvim'
     -- which-key
     use {
         "folke/which-key.nvim",
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,32 +9,41 @@ keymap('n', 'sl', ':vsplit<CR>', opts)
 keymap('n', 'sk', ':split<CR>', opts)
 keymap('n', '<LEADER><CR>', ':nohlsearch<CR>', opts)
 
+keymap('n', '<A-w>', '<C-w>', opts)
+
+
+
+--------------------NvimTree-------------------------------
 keymap('n','<LEADER>e', ':NvimTreeToggle<ENTER>', opts) -- open nvimtree
 keymap('n','th', ':BufferLineCyclePrev<CR>', opts) -- switch tab
 keymap('n','tl', ':BufferLineCycleNext<CR>', opts)
+
+
+---------------------Joshuto------------------------
 keymap('n', '<LEADER>jo', ':Joshuto<ENTER>', opts)
 
+--------------------BarBar---------------------------
 -- Move to previous/next
--- keymap('n', '<LEADER>,', '<Cmd>BufferPrevious<CR>', opts)
--- keymap('n', '<LEADER>.', '<Cmd>BufferNext<CR>', opts)
--- Re-orde<LEADER>oprevious/next
--- keymap('n', '<LEADER><', '<Cmd>BufferMovePrevious<CR>', opts)
--- keymap('n', '<LEADER>>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto bu<LEADER>rin position...
-keymap('n', '<LEADER>1', '<Cmd>BufferGoto 1<CR>', opts)
-keymap('n', '<LEADER>2', '<Cmd>BufferGoto 2<CR>', opts)
-keymap('n', '<LEADER>3', '<Cmd>BufferGoto 3<CR>', opts)
-keymap('n', '<LEADER>4', '<Cmd>BufferGoto 4<CR>', opts)
-keymap('n', '<LEADER>5', '<Cmd>BufferGoto 5<CR>', opts)
-keymap('n', '<LEADER>6', '<Cmd>BufferGoto 6<CR>', opts)
-keymap('n', '<LEADER>7', '<Cmd>BufferGoto 7<CR>', opts)
-keymap('n', '<LEADER>8', '<Cmd>BufferGoto 8<CR>', opts)
-keymap('n', '<LEADER>9', '<Cmd>BufferGoto 9<CR>', opts)
-keymap('n', '<LEADER>0', '<Cmd>BufferLast<CR>', opts)
--- Pin/unp<LEADER>bffer
--- keymap('n', '<LEADER>p', '<Cmd>BufferPin<CR>', opts)
--- Close b<LEADER>e
-keymap('n', '<LEADER>q', '<Cmd>BufferClose<CR>', opts)
+keymap('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+keymap('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- key-- Re-order to previous/next
+keymap('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+keymap('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- key-- Goto buffer in position...
+keymap('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+keymap('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+keymap('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+keymap('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+keymap('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+keymap('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+keymap('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+keymap('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- key-- Pin/unpin buffer
+keymap('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- key-- Close buffer
+keymap('n', '<A-q>', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
@@ -43,17 +52,27 @@ keymap('n', '<LEADER>q', '<Cmd>BufferClose<CR>', opts)
 --                 :BufferCloseAllButCurrentOrPinned
 --                 :BufferCloseBuffersLeft
 --                 :BufferCloseBuffersRight
--- Magic buffer-picking mode
+-- keyMagic buffer-picking mode
 keymap('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- Sort automatically by...
+-- key-- Sort automatically by...
 keymap('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
 keymap('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
 keymap('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
 keymap('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+--
+-- -- Other:
+-- -- :BarbarEnable - enables barbar (enabled by default)
+-- -- :BarbarDisable - very bad command, should never be used
 
+----------------------SymbolsOutline--------------------------------
 keymap('n', '<LEADER>s', ':SymbolsOutline<CR>', opts)
+----------------------Open Console --------------------------------
 keymap('n', '<LEADER>c', ':FloatermNew<CR>', opts)
 
+-----------------------Find File -------------------------------
 keymap('n', '<LEADER>ff', ':Telescope find_files<CR>', opts)
+-----------------------Registers History----------------------------
 keymap('n', '<LEADER>rr', ':Telescope registers<CR>', opts)
+-----------------------MarkdownPreview----------------------------
+keymap('n', '<LEADER>md', ':MarkdownPreview<CR>', opts)
 
